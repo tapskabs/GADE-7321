f5e0f73328a5736481c0ae015dfd66136d119f1c
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2025-06-06 14:47:57 +0200

    edits and changes

--- Assets/Scripts/CustomHashMap.cs
+++ Assets/Scripts/CustomHashMap.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomHashMap : MonoBehaviour
+public class CustomHashMap 
 {
     private class KeyValue
     {
