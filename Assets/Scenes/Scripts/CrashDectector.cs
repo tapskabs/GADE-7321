77c86622a41fcc8873b3938ebf28026bccf35b5c
Tapuwa Kabwato <141030379+tapskabs@users.noreply.github.com>
2025-06-09 22:52:01 +0200

    Finale

--- Assets/Scenes/Scripts/CrashDectector.cs
+++ Assets/Scenes/Scripts/CrashDectector.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class CrashDectector : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision) //Tag detection instead of mesh detection......
+    private void OnCollisionEnter(Collision collision) 
     {
         if (collision.collider.CompareTag("Wall"))
         {
             SFXManager.Instance.PlaySFX("crash");
-            //Make sure sound references are in the SFX sound manager and naming conventions
-            //Make sure auido folder is in place
+            
         }
     }
 }
